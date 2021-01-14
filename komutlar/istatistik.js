109c67e7807580405edffdc1e31b7834f9974040
Glitch (juvenile-amazing-bow) <none>
2021-01-14 14:42:24 +0000

    🐔🗾 Checkpoint ./komutlar/müzik.js:27393991/1076 ./komutlar/istatistik.js:27393991/2443

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,26 +1,37 @@
 const Discord = require("discord.js");
 const moment = require("moment");
+const os = require('os');
 require("moment-duration-format");
-
-exports.run = (client, msg) => {
-  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-  msg.channel.sendCode("asciidoc", `= istatistikler =
-- Bellek kullanımı :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-- Kullanıcılar     :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
-- Sunucular        :: ${client.guilds.size.toLocaleString()}
-- Kanallar         :: ${client.channels.size.toLocaleString()}
-- Ping             :: ${client.ping}`);
-};
+exports.run = async (bot, message, args) => {
+   const csure = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const botbilgi = new Discord.RichEmbed()
+  .setColor('#e30b00')
+   .setAuthor(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setFooter('Bessinger Bot \'Buyur benim istatistiklerim', bot.user.avatarURL)
+  .addField("» **Botun Sahibi**", "[bessinger#9049]")
+  .addField("»  **Geliştirici** ","[bessinger#9049]")
+  .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
+  .addField("» **Çalışma süresi**", csure)
+  .addField("» **Kullanıcılar**" , bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
+  .addField("» **Sunucular**", bot.guilds.size.toLocaleString(), true)
+  .addField("» **Kanallar**", bot.channels.size.toLocaleString(), true)
+  .addField("» **Discord.JS sürüm**", "v"+Discord.version, true)
+  .addField("» **Node.JS sürüm**", `${process.version}`, true)
+  .addField("» **Ping**", bot.ping+" ms", true)
+ 
+   
+ return message.channel.send(botbilgi);
+  };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['bot durum', 'i', 'bi', 'istatistikler', 'kullanımlar', 'botdurum', 'bd', 'istatisik', 'stats', 'stat'],
+  aliases: [ 'istatistik', 'bot-bilgi', 'bot-istatistik'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'istatistik',
-  description: 'Botun istatistik gösterir.',
-  usage: 'istatistik'
+  name: "istatistik",
+  description: "Botun Bilgilerini Gösterir.",
+  usage: "istatistik"
 };
