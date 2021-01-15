792379f2e90d28e56d61093cd446f984d36b8cb6
Glitch (juvenile-amazing-bow) <none>
2021-01-15 13:36:40 +0000

    🚣👦 Checkpoint ./komutlar/istatistik.js:27393991/3057

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -3,35 +3,33 @@ const moment = require("moment");
 const os = require('os');
 require("moment-duration-format");
 exports.run = async (bot, message, args) => {
-   const csure = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-   const botbilgi = new Discord.RichEmbed()
-  .setColor('#e30b00')
-   .setAuthor(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
-  .setFooter('Bessinger Bot \'Buyur benim istatistiklerim', bot.user.avatarURL)
-  .addField("» **Botun Sahibi**", "[bessinger#9049]")
-  .addField("»  **Geliştirici** ","[bessinger#9049]")
-  .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
-  .addField("» **Çalışma süresi**", csure)
+   const çalışmasüresi = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const lewixcode = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setFooter('Lewix Bot  \'İstatistikler', bot.user.avatarURL)
+  .addField("»  **Geliştirici** ","<@676886594581692427>")
+  .addField("» **Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true) 
+  .addField("» **Çalışma süresi**", çalışmasüresi)
   .addField("» **Kullanıcılar**" , bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
   .addField("» **Sunucular**", bot.guilds.size.toLocaleString(), true)
   .addField("» **Kanallar**", bot.channels.size.toLocaleString(), true)
   .addField("» **Discord.JS sürüm**", "v"+Discord.version, true)
   .addField("» **Node.JS sürüm**", `${process.version}`, true)
   .addField("» **Ping**", bot.ping+" ms", true)
- 
-   
- return message.channel.send(botbilgi);
+  .addField("» **CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+  .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](https://discord.gg/zg4uj5t9H2)", )
+ return message.channel.send(lewixcode);
   };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [ 'istatistik', 'bot-bilgi', 'bot-istatistik'],
+  aliases: [ 'istatistik', 'bot-istatistik'],
   permLevel: 0
 };
 
 exports.help = {
   name: "istatistik",
-  description: "Botun Bilgilerini Gösterir.",
+  description: "istatistikler",
   usage: "istatistik"
 };
